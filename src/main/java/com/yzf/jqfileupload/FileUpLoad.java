4fb2a916fd293259977ea0d314614eac1b51a746
yzf <445220270@qq.com>
2016-10-28 17:20:34 +0800

    创建AJAX通用文件上传Dome

---
+++ src/main/java/com/yzf/jqfileupload/FileUpLoad.java
@@ -0,0 +1,20 @@
+package com.yzf.jqfileupload;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+/**
+ * Created by YZF on 2016/10/28.
+ */
+public class FileUpLoad extends HttpServlet{
+
+    public static final String fileFolder="";
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+    }
+}
