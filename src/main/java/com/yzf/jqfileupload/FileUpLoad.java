c59819b918900ee1b080e80fe003e9a2447af0df
yzf <445220270@qq.com>
2016-10-29 16:03:09 +0800

    16_10_29 实现创建上传文件夹

--- src/main/java/com/yzf/jqfileupload/FileUpLoad.java
+++ src/main/java/com/yzf/jqfileupload/FileUpLoad.java
@@ -4,17 +4,30 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by YZF on 2016/10/28.
  */
 public class FileUpLoad extends HttpServlet{
 
-    public static final String fileFolder="";
-
+    /**
+     * 处理上传方法
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-
+        final String FIle_PATH=request.getRealPath("");
+        File upload_file=new File(FIle_PATH + "/UserFiles/" + new SimpleDateFormat("yyyyMMdd").format(new Date()));
+        if(!upload_file.exists()){
+            upload_file.mkdirs();
+        }
+        response.getWriter().print(upload_file.getPath());
     }
 }
